43c15e0681ed3422da5fe77ce42dbd6371d56837
Denis <carriere.denis@gmail.com>
2017-08-15 21:45:59 -0400

    Change map to HTTPS

--- js/map.js
+++ js/map.js
@@ -8,6 +8,6 @@ const options = {
 }
 const map = L.map('map', options).setView([45.4215, -75.6972], 14)
 
-L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+L.tileLayer('https://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://osm.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map)
