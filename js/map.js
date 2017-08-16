c50eb5beca55361da4f4b9c260a3b3bf02606ca1
Denis <carriere.denis@gmail.com>
2017-08-15 21:47:22 -0400

    Change Map tiles

--- js/map.js
+++ js/map.js
@@ -8,6 +8,6 @@ const options = {
 }
 const map = L.map('map', options).setView([45.4215, -75.6972], 14)
 
-L.tileLayer('https://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://osm.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map)
